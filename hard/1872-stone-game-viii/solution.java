40a953b94b29e9b0f7e17e373f8e8cd0d43447e5
AbdulWahith18 <abdulwahith0818@gmail.com>
2026-08-24 18:02:16 +0530

    ✅ Stone Game VIII | Hard | Java | Runtime: 2 ms | Memory: 42.7 MB | 2026-08-24

--- hard/1872-stone-game-viii/solution.java
+++ hard/1872-stone-game-viii/solution.java
@@ -1,13 +1,7 @@
-        for (int i = n - 2; i >= 1; i--) {
-        // Try every possible point where the game can be split
-
-        int ans = prefix[n - 1];
-        // Initially, consider taking all stones
-
-        }
             prefix[i] = prefix[i - 1] + s[i];
-
+        }
+        for(int i:s)
         for (int i = 1; i < n; i++) {
-        prefix[0] = s[0];
+
         int[] prefix = new int[n];
-        // Calculate prefix sums
+        prefix[0] = s[0];
