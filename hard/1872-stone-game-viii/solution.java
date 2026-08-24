9ab9cbe6e2159cae4585c60d6f2c272d4f49dfd4
AbdulWahith18 <abdulwahith0818@gmail.com>
2026-08-24 18:02:40 +0530

    ✅ Stone Game VIII | Hard | Java | Runtime: 2 ms | Memory: 42.7 MB | 2026-08-24

--- hard/1872-stone-game-viii/solution.java
+++ hard/1872-stone-game-viii/solution.java
@@ -1,7 +1,6 @@
             prefix[i] = prefix[i - 1] + s[i];
-        }
-        for(int i:s)
         for (int i = 1; i < n; i++) {
 
         int[] prefix = new int[n];
         prefix[0] = s[0];
+        }
