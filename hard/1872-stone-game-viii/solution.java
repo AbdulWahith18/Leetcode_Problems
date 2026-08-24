cad7639193dee8802b30087a7e1fa57854406399
AbdulWahith18 <abdulwahith0818@gmail.com>
2026-08-24 17:54:44 +0530

    ✅ Stone Game VIII | Hard | Java | Runtime: 4 ms | Memory: 42.7 MB | 2026-08-24

--- hard/1872-stone-game-viii/solution.java
+++ hard/1872-stone-game-viii/solution.java
@@ -1,10 +1,9 @@
-            if(prefix[i]>1&&prefix[i]>max){
+        for(int i=l-2;i>=0;i--)
+        {
+            if(prefix[i]>1){
                 in=i;
                 max=prefix[i];
             }
         }
         if(in==l-1)
             return max;
-        {
-        for(int i=l-2;i>=0;i--)
-        int max=prefix[l-1],in=l-1;
