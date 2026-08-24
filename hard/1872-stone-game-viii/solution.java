0fa7d117a79f3a5009ea80d88aece79e870f5052
AbdulWahith18 <abdulwahith0818@gmail.com>
2026-08-24 17:49:23 +0530

    ✅ Stone Game VIII | Hard | Java | Runtime: 4 ms | Memory: 42.8 MB | 2026-08-24

--- hard/1872-stone-game-viii/solution.java
+++ hard/1872-stone-game-viii/solution.java
@@ -1,2 +1,6 @@
-                max=prefix[i];
-            }
+        // if(prefix[l-1]>prefix[l-2])
+        }
+            prefix[i]=prefix[i-1]+s[i];
+        {
+        for(int i:prefix)
+            System.out.println(i+" ");
