7c9c927b8740cc541802a2e973a49d4bf2316ea0
AbdulWahith18 <abdulwahith0818@gmail.com>
2026-08-25 18:43:49 +0530

    ✅ Intersection of Two Arrays | Easy | Java | Runtime: 0 ms | Memory: 42.8 MB | 2026-08-25

--- easy/0349-intersection-of-two-arrays/solution.java
+++ easy/0349-intersection-of-two-arrays/solution.java
@@ -1,20 +1,26 @@
-        {
-        while(i<x&&j<y)
-            if(i<x)
-                h1.add(nums1[i++]);
-            if(j<y)
-                h2.add(nums2[j++]);
-        }
-        List<Integer> l=new ArrayList<>();
-        for(int k=0;k<x;k++)
-        {
-            if(h1.contains(nums1[k])&&h2.contains(nums1[k])&&!l.contains(nums1
-                l.add(nums1[k]);
-        }
-        int res[] = new int[l.size()];
-        for(int k=0;k<l.size();k++)
-            res[k]=l.get(k);
-        return res;
-    }
-            [k]))
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+
+        HashSet<Integer> h1=new HashSet<>();
+        HashSet<Integer> h2=new HashSet<>();
+        int i=0,j=0;
+        int x=nums1.length,y=nums2.length;
+        while(i<x&&j<y)
+        {
+            if(i<x)
+                h1.add(nums1[i++]);
+            if(j<y)
+                h2.add(nums2[j++]);
+        }
+        List<Integer> l=new ArrayList<>();
+        for(int k=0;k<x;k++)
+        {
+            if(h1.contains(nums1[k])&&h2.contains(nums1[k])&&!l.contains(nums1[k]))
+                l.add(nums1[k]);
+        }
+        int res[] = new int[l.size()];
+        for(int k=0;k<l.size();k++)
+            res[k]=l.get(k);
+        return res;
+    }
 }
