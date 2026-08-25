fb970eca1c7588750a1b639bfc54151b2acd6e93
AbdulWahith18 <abdulwahith0818@gmail.com>
2026-08-25 18:43:02 +0530

    ✅ Intersection of Two Arrays | Easy | Java | Runtime: 0 ms | Memory: 42.6 MB | 2026-08-25

--- easy/0349-intersection-of-two-arrays/solution.java
+++ easy/0349-intersection-of-two-arrays/solution.java
@@ -1,21 +1,20 @@
+        {
+        while(i<x&&j<y)
             if(i<x)
-        }
                 h1.add(nums1[i++]);
             if(j<y)
                 h2.add(nums2[j++]);
+        }
         List<Integer> l=new ArrayList<>();
         for(int k=0;k<x;k++)
         {
-            if(h1.contains(nums1[k])&&h2.contains(nums1[k]))
+            if(h1.contains(nums1[k])&&h2.contains(nums1[k])&&!l.contains(nums1
                 l.add(nums1[k]);
-        {
-        while(i<x&&j<y)
-        int x=nums1.length,y=nums2.length;
-        int i=0,j=0;
-        HashSet<Integer> h1=new HashSet<>();
-    public int[] intersection(int[] nums1, int[] nums2) {
-
-        HashSet<Integer> h2=new HashSet<>();
         }
         int res[] = new int[l.size()];
         for(int k=0;k<l.size();k++)
+            res[k]=l.get(k);
+        return res;
+    }
+            [k]))
+}
