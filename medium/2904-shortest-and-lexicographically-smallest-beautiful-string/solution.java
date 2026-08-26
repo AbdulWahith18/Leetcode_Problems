0a08c31f1842ee4a80176428463664ba531520aa
AbdulWahith18 <abdulwahith0818@gmail.com>
2026-08-26 09:15:28 +0530

    ✅ Shortest and Lexicographically Smallest Beautiful String | Medium | Java | Runtime: 0 ms | Memory: 42.8 MB | 2026-08-26

--- medium/2904-shortest-and-lexicographically-smallest-beautiful-string/solution.java
+++ medium/2904-shortest-and-lexicographically-smallest-beautiful-string/solution.java
@@ -1,17 +1,34 @@
+class Solution {
+    public String shortestBeautifulSubstring(String s, int k) {
 
-                r++;
-            }
+        int n = s.length();
+        char[] str = s.toCharArray();
 
-            if (count == k && (r - l) < min) {
-                min = r - l;
-                x = l;
-                y = r - 1;
-            }
-        }
+        int min = Integer.MAX_VALUE;
+        int x = -1, y = -1;
 
-        if (x == -1)
-            return "";
+        for (int l = 0; l < n; l++) {
 
-        return s.substring(x, y + 1);
-    }
+            int count = 0, r = l;
+
+            while (count < k && r < n)
+            {
+                if (str[r] == '1')
+                    count++;
+                r++;
+            }
+
+            if (count == k && (r - l) < min) 
+            {
+                min = r - l;
+                x = l;
+                y = r - 1;
+            }
+        }
+
+        if (x == -1)
+            return "";
+
+        return s.substring(x, y + 1);
+    }
 }
