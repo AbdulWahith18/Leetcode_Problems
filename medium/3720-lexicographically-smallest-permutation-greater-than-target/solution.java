71c078401059620f42fa6009c5ec689b40cf285d
AbdulWahith18 <abdulwahith0818@gmail.com>
2026-08-27 07:03:36 +0530

    ✅ Lexicographically Smallest Permutation Greater Than Target | Medium | Java | Runtime: 3 ms (58.06%) | Memory: 46.8 MB | 2026-08-27

--- medium/3720-lexicographically-smallest-permutation-greater-than-target/solution.java
+++ medium/3720-lexicographically-smallest-permutation-greater-than-target/solution.java
@@ -9,72 +9,19 @@ class Solution {
             freq[c - 'a']++;
         }
 
-        char[] ans = new char[n];
+        // Store the best (rightmost) position where
+        // we can make target[i] larger.
+        int bestPos = -1;
+        int bestChar = -1;
+
+        // We will store the frequency array at every position
+        // before consuming target[i].
+        int[][] states = new int[n][26];
 
         for (int i = 0; i < n; i++) {
 
-            int t = target.charAt(i) - 'a';
-
-            // First, try to keep target[i] equal
-            if (freq[t] > 0) {
-
-                ans[i] = target.charAt(i);
-                freq[t]--;
-
-            } else {
-
-                // Cannot keep equality.
-                // Find smallest character greater than target[i].
-                int bigger = -1;
-
-                for (int c = t + 1; c < 26; c++) {
-                    if (freq[c] > 0) {
-                        bigger = c;
-                        break;
-                    }
-                }
-
-                if (bigger != -1) {
-
-                    ans[i] = (char) ('a' + bigger);
-                    freq[bigger]--;
-
-                    // Fill remaining characters in sorted order
-                    int pos = i + 1;
-
-                    for (int c = 0; c < 26; c++) {
-                        while (freq[c] > 0) {
-                            ans[pos++] = (char) ('a' + c);
-                            freq[c]--;
-                        }
-                    }
-
-                    return new String(ans);
-                }
-
-                /*
-                 * We cannot continue matching target.
-                 * Need to backtrack.
-                 */
-                break;
-            }
-        }
-
-        // target prefix may need to be changed.
-        // Rebuild frequencies.
-        freq = new int[26];
-
-        for (char c : s.toCharArray()) {
-            freq[c - 'a']++;
-        }
-
-        // Try changing target from right to left
-        for (int i = n - 1; i >= 0; i--) {
-
-            // Use target[0 ... i-1]
-            for (int j = 0; j < i; j++) {
-                freq[target.charAt(j) - 'a']--;
-            }
+            // Save available characters before using target[i]
+            states[i] = freq.clone();
 
             int t = target.charAt(i) - 'a';
 
@@ -82,34 +29,49 @@ class Solution {
             for (int c = t + 1; c < 26; c++) {
 
                 if (freq[c] > 0) {
-
-                    ans = new char[n];
-
-                    // Prefix
-                    for (int j = 0; j < i; j++) {
-                        ans[j] = target.charAt(j);
-                    }
-
-                    ans[i] = (char) ('a' + c);
-                    freq[c]--;
-
-                    int pos = i + 1;
-
-                    for (int x = 0; x < 26; x++) {
-                        while (freq[x] > 0) {
-                            ans[pos++] = (char) ('a' + x);
-                            freq[x]--;
-                        }
-                    }
-
-                    return new String(ans);
+                    bestPos = i;
+                    bestChar = c;
+                    break;
                 }
             }
 
-            // Restore target[i]
-            freq[t]++;
+            // We need target[i] to continue matching
+            if (freq[t] == 0) {
+                break;
+            }
+
+            freq[t]--;
         }
 
-        return "";
+        // No position can make the string greater
+        if (bestPos == -1) {
+            return "";
+        }
+
+        // Reconstruct answer
+        StringBuilder ans = new StringBuilder();
+
+        // Prefix same as target
+        ans.append(target.substring(0, bestPos));
+
+        // Put smallest possible greater character
+        ans.append((char) ('a' + bestChar));
+
+        // Get the frequency state before bestPos
+        freq = states[bestPos].clone();
+
+        // Remove the character we just used
+        freq[bestChar]--;
+
+        // Fill remaining positions in sorted order
+        for (int c = 0; c < 26; c++) {
+
+            while (freq[c] > 0) {
+                ans.append((char) ('a' + c));
+                freq[c]--;
+            }
+        }
+
+        return ans.toString();
     }
 }
