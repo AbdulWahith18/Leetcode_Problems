97200b6ebaad761c177913ce2bb71c6ff01d6b47
AbdulWahith18 <abdulwahith0818@gmail.com>
2026-08-28 07:43:05 +0530

    ✅ Lexicographically Smallest Palindromic Permutation Greater Than Target | Hard | Java | Runtime: 1 ms | Memory: 42.7 MB | 2026-08-28

--- hard/3734-lexicographically-smallest-palindromic-permutation-greater-than-target/solution.java
+++ hard/3734-lexicographically-smallest-palindromic-permutation-greater-than-target/solution.java
@@ -5,13 +5,3 @@
             leftHalf.append(oddChar);
             leftHalf.append(leftHalf.reverse());
         }
-        else
-            leftHalf.append(leftHalf.reverse());
-
-        String rev=leftHalf.toString();
-        int com=rev.compareTo(target);
-        if(com>0)
-            return rev;
-        return "";
-            
-    }
