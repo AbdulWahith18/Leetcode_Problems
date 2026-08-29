5f813bfcb1cceffaafde559031921430ee1dda7c
AbdulWahith18 <abdulwahith0818@gmail.com>
2026-08-29 09:24:12 +0530

    ✅ Make Lexicographically Smallest Array by Swapping Elements | Medium | Java | Runtime: 0 ms | Memory: 43.1 MB | 2026-08-29

--- medium/2948-make-lexicographically-smallest-array-by-swapping-elements/solution.java
+++ medium/2948-make-lexicographically-smallest-array-by-swapping-elements/solution.java
@@ -1,11 +1,24 @@
-class Solution {
-    public int[] lexicographicallySmallestArray(int[] nums, int limit) {
-        int n=nums.length;
-        int i=0,j=1;
-        while(j<n)
-        {
-            if(nums[i]<=nums[j])
-            {
-                i++;
-                j++;
-                continue;
+class Solution {
+    public int[] lexicographicallySmallestArray(int[] nums, int limit) {
+        int n=nums.length;
+        int i=0,j=1;
+        while(j<n)
+        {
+            if(nums[i]<=nums[j])
+            {
+                i++;
+                j++;
+                continue;
+            }
+            if((nums[i]-nums[j])<=limit)
+            {
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+            }
+            i++;
+            j++;
+        }
+        return nums;
+    }
+}
