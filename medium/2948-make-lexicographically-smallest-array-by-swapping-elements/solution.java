d78ec62b3a5a01155b4bba13ae0e0a36310a2eda
AbdulWahith18 <abdulwahith0818@gmail.com>
2026-08-29 09:43:07 +0530

    ✅ Make Lexicographically Smallest Array by Swapping Elements | Medium | Java | Runtime: 2 ms | Memory: 42.9 MB | 2026-08-29

--- medium/2948-make-lexicographically-smallest-array-by-swapping-elements/solution.java
+++ medium/2948-make-lexicographically-smallest-array-by-swapping-elements/solution.java
@@ -1,24 +1,83 @@
 class Solution {
     public int[] lexicographicallySmallestArray(int[] nums, int limit) {
-        int n=nums.length;
-        int i=0,j=1;
-        while(j<n)
-        {
-            if(nums[i]<=nums[j])
-            {
-                i++;
+        // int n=nums.length;
+        // int i=0;
+        // // while(j<n)
+        // // {
+        // //     if(nums[i]<=nums[j])
+        // //     {
+        // //         i++;
+        // //         j++;
+        // //         continue;
+        // //     }
+        // //     if((nums[i]-nums[j])<=limit)
+        // //     {
+        // //         int temp=nums[i];
+        // //         nums[i]=nums[j];
+        // //         nums[j]=temp;
+        // //     }
+        // //     i++;
+        // //     j++;
+        // // }
+        // while(i<n-1)
+        // {
+        //     int j=i+1;
+        //     int last=j;
+        //     while(j<n)
+        //     {
+        //         if(nums[i]<=nums[j]||(nums[i]-nums[j])>limit)
+        //         {
+        //             j++;
+        //             continue;
+        //         }
+        //         if((nums[i]-nums[j])<=limit)
+        //         {
+        //             int temp=nums[i];
+        //             nums[i]=nums[j];
+        //             nums[j]=temp;
+        //             last=j;
+        //             j++;
+        //         }
+        //     }
+        //     i=last;
+        // }
+        // return nums;
+                int n = nums.length;
+
+        int i = 0;
+
+        while (i < n - 1) {
+
+            int j = i + 1;
+            int last = i;
+
+            while (j < n) {
+
+                // If nums[j] can make position i smaller
+                if (nums[j] < nums[i] &&
+                    nums[i] - nums[j] <= limit) {
+
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+
+                    last = j;
+                }
+
                 j++;
+            }
+
+            /*
+             * If we changed nums[i], start checking again
+             * because an even smaller value may now be usable.
+             */
+            if (last != i) {
                 continue;
             }
-            if((nums[i]-nums[j])<=limit)
-            {
-                int temp=nums[i];
-                nums[i]=nums[j];
-                nums[j]=temp;
-            }
+
             i++;
-            j++;
         }
+
         return nums;
     }
 }
