1d327cbde3a69c6589e5dddd0454209a56aa5fb3
AbdulWahith18 <abdulwahith0818@gmail.com>
2026-08-30 11:35:27 +0530

    ✅ Find Missing Observations | Medium | Java | Runtime: 0 ms | Memory: 43 MB | 2026-08-30

--- medium/2028-find-missing-observations/solution.java
+++ medium/2028-find-missing-observations/solution.java
@@ -1,7 +1,26 @@
-            arrSum+=rolls[i];
+            }
+            else if((missSum-3)>0)
+            {
+                res[j++]=3;
+                missSum-=3;
+            }
+                missSum-=4;
+                res[j++]=4;
+            {
+            else if((missSum-4)>0)
+            }
+                missSum-=5;
+                res[j++]=5;
+            else if((missSum-2)>0)
+            {
+                res[j++]=2;
+                missSum-=2;
+            }
+            else
+            {
+                res[j++]=1;
+                missSum-=1;
+            }
         }
-        int missSum=mean*(rolls.length+n)-arrSum;
-    }
-        System.out.println(missSum);
-        return new int[n];
-}
+        return res;
+            k--;
