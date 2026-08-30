4799b0ad8a05d28758af6f6ea6ecca1a860af9d3
AbdulWahith18 <abdulwahith0818@gmail.com>
2026-08-30 07:51:15 +0530

    ✅ Removing Minimum and Maximum From Array | Medium | Java | Runtime: 2 ms (100.00%) | Memory: 86.7 MB | 2026-08-30

--- medium/2091-removing-minimum-and-maximum-from-array/solution.java
+++ medium/2091-removing-minimum-and-maximum-from-array/solution.java
@@ -1,12 +1,39 @@
-        // Both from front
-        int front = right + 1;
+class Solution {
+    public int minimumDeletions(int[] nums) {
 
-        // Both from back
-        int back = l - left;
+        int max = nums[0], min = nums[0];
+        int m = 0, n = 0;
 
-        // One from front, one from back
-        int mixed = (left + 1) + (l - right);
+        int l = nums.length;
 
-        return Math.min(front, Math.min(back, mixed));
-    }
+        for (int i = 1; i < l; i++) {
+
+            if (nums[i] > max) {
+                max = nums[i];
+                m = i;
+            }
+
+            if (nums[i] < min) {
+                min = nums[i];
+                n = i;
+            }
+        }
+
+        // m = max index
+        // n = min index
+
+        int left = Math.min(m, n);
+        int right = Math.max(m, n);
+
+        // Both from front
+        int front = right + 1;
+
+        // Both from back
+        int back = l - left;
+
+        // One from front, one from back
+        int mixed = (left + 1) + (l - right);
+
+        return Math.min(front, Math.min(back, mixed));
+    }
 }
