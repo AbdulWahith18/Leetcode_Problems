cc4be40fe3161241a0e0349859e66492db3a7f9c
AbdulWahith18 <abdulwahith0818@gmail.com>
2026-08-30 14:27:00 +0530

    ✅ Find Missing and Repeated Values | Easy | Java | Runtime: 6 ms (27.58%) | Memory: 47.7 MB | 2026-08-30

--- easy/2965-find-missing-and-repeated-values/solution.java
+++ easy/2965-find-missing-and-repeated-values/solution.java
@@ -1,18 +1,24 @@
-        int rep=0;
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<m;j++)
-        }
-            {
-                if(s.contains(grid[i][j]))
-            }
-                    rep=grid[i][j];
-                s.add(grid[i][j]);
-        int sum=0;
-        for(Integer a:s)
-            sum+=a;
-        int total=(n*(n+1))/2;
-        int n=m*m;
-        int New=total-sum;
-        return new int[]{rep,New};
+class Solution {
+    public int[] findMissingAndRepeatedValues(int[][] grid) {
+        int m=grid.length;
+        Set<Integer> s=new HashSet<>();
+        int rep=0;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(s.contains(grid[i][j]))
+                    rep=grid[i][j];
+                s.add(grid[i][j]);
+            }
+        }
+        int sum=0;
+        for(Integer a:s)
+            sum+=a;
+        int n=m*m;
+        int total=(n*(n+1))/2;
+        int New=total-sum;
+        return new int[]{rep,New};
 
+    }
+}
