0cb529e72032bfa04bfa5dd461d7c6f56c167a63
AbdulWahith18 <abdulwahith0818@gmail.com>
2026-08-30 11:35:59 +0530

    ✅ Find Missing Observations | Medium | Java | Runtime: 0 ms | Memory: 43.1 MB | 2026-08-30

--- medium/2028-find-missing-observations/solution.java
+++ medium/2028-find-missing-observations/solution.java
@@ -7,20 +7,5 @@
                 missSum-=4;
                 res[j++]=4;
             {
-            else if((missSum-4)>0)
-            }
-                missSum-=5;
-                res[j++]=5;
             else if((missSum-2)>0)
             {
-                res[j++]=2;
-                missSum-=2;
-            }
-            else
-            {
-                res[j++]=1;
-                missSum-=1;
-            }
-        }
-        return res;
-            k--;
