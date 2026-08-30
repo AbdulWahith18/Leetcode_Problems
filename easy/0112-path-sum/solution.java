139258604b22c5651305d1593d22b20e4c93e750
AbdulWahith18 <abdulwahith0818@gmail.com>
2026-08-30 20:52:51 +0530

    ✅ Path Sum | Easy | Java | Runtime: 0 ms (100.00%) | Memory: 44.8 MB | 2026-08-30

--- easy/0112-path-sum/solution.java
+++ easy/0112-path-sum/solution.java
@@ -1,17 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
 
-        if (root == null)
-            return false;
+        if (root == null)
+            return false;
 
-        if (root.left == null && root.right == null) {
-            return targetSum == root.val;
-    public boolean hasPathSum(TreeNode root, int targetSum) {
-        }
+        if (root.left == null && root.right == null) {
+            return targetSum == root.val;
+        }
 
-        targetSum -= root.val;
+        targetSum -= root.val;
 
-        return hasPathSum(root.left, targetSum) ||
-               hasPathSum(root.right, targetSum);
- */
-class Solution {
-    }
+        return hasPathSum(root.left, targetSum) ||
+               hasPathSum(root.right, targetSum);
+    }
 }
