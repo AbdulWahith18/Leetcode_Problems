e70c2ca6856ffb95132f0578e0a2ccb29ec3a36a
AbdulWahith18 <abdulwahith0818@gmail.com>
2026-08-31 06:56:21 +0530

    ✅ Find the Minimum and Maximum Number of Nodes Between Critical Points | Medium | Java | Runtime: 9 ms (23.01%) | Memory: 107.6 MB | 2026-08-31

--- medium/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/solution.java
+++ medium/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/solution.java
@@ -1,24 +1,51 @@
-            if(temp.next != null &&
-               ((temp.val > prev.val && temp.val > temp.next.val) ||
-        while(temp != null) {
-
-
-        ListNode temp = head.next;
-        ListNode prev = head;
-
-        List<Integer> l = new ArrayList<>();
-
-        int i = 1;
-    public int[] nodesBetweenCriticalPoints(ListNode head) {
-
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
- *     ListNode(int val) { this.val = val; }
- *     ListNode() {}
- *     int val;
- *     ListNode next;
- * public class ListNode {
 /**
- * Definition for singly-linked list.
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int[] nodesBetweenCriticalPoints(ListNode head) {
+
+        List<Integer> l = new ArrayList<>();
+
+        int i = 1;
+
+        ListNode prev = head;
+        ListNode temp = head.next;
+
+        while(temp != null) {
+
+            if(temp.next != null &&
+               ((temp.val > prev.val && temp.val > temp.next.val) ||
+                (temp.val < prev.val && temp.val < temp.next.val))) {
+
+                l.add(i);
+            }
+
+            prev = temp;
+            temp = temp.next;
+            i++;
+        }
+
+        if(l.size() < 2)
+            return new int[]{-1, -1};
+
+        int max = l.get(l.size() - 1) - l.get(0);
+
+        int min = Integer.MAX_VALUE;
+
+        for(int j = 1; j < l.size(); j++) {
+
+            int distance = l.get(j) - l.get(j - 1);
+
+            min = Math.min(min, distance);
+        }
+
+        return new int[]{min, max};
+    }
+}
