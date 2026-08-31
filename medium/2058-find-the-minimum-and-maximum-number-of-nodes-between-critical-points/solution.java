35af923ba93137ded1685394a181e29ac152998e
AbdulWahith18 <abdulwahith0818@gmail.com>
2026-08-31 06:46:56 +0530

    ✅ Find the Minimum and Maximum Number of Nodes Between Critical Points | Medium | Java | Runtime: 0 ms | Memory: 43 MB | 2026-08-31

--- medium/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/solution.java
+++ medium/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/solution.java
@@ -8,18 +8,3 @@
         {
             if(temp.next!=null&&temp.val>temp.next.val&&temp.val>prev.val)
                 l.add(i);
-            else if(temp.next!=null&&temp.val<temp.next.val&&temp.val<prev.val)
-                l.add(i);
-            prev=temp;
-            temp=temp.next;
-            i++;
-        }
-        for(Integer j:l)
-            System.out.println(j);
-        if(l.size()==1)
-            return new int[]{-1,-1};
-        int min=l.get(2)-l.get(1),max=l.get(l.size()-1)-l.get(0);
-        return new int[]{min,max};
-        
-    }
-}
