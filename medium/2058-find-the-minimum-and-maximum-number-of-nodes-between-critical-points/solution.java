192050267207bf3222fa831419ef178e086ec5de
AbdulWahith18 <abdulwahith0818@gmail.com>
2026-08-31 06:56:12 +0530

    ✅ Find the Minimum and Maximum Number of Nodes Between Critical Points | Medium | Java | Runtime: 0 ms | Memory: 42.9 MB | 2026-08-31

--- medium/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/solution.java
+++ medium/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/solution.java
@@ -1,6 +1,24 @@
-        ListNode temp=head.next;
-        while(temp!=null)
-        {
-            if(temp.next!=null&&temp.val>temp.next.val&&temp.val>prev.val)
-                l.add(i);
-            else if(temp.next!=null&&temp.val<temp.next.val&&temp.val<prev.val)
+            if(temp.next != null &&
+               ((temp.val > prev.val && temp.val > temp.next.val) ||
+        while(temp != null) {
+
+
+        ListNode temp = head.next;
+        ListNode prev = head;
+
+        List<Integer> l = new ArrayList<>();
+
+        int i = 1;
+    public int[] nodesBetweenCriticalPoints(ListNode head) {
+
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode() {}
+ *     int val;
+ *     ListNode next;
+ * public class ListNode {
+/**
+ * Definition for singly-linked list.
