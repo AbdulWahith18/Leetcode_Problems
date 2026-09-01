0b75facc61f7421522e07aa006e6701e571e2e5d
AbdulWahith18 <abdulwahith0818@gmail.com>
2026-09-01 07:03:51 +0530

    ✅ Minimum Moves to Clean the Classroom | Medium | Java | Runtime: 6 ms | Memory: 46.4 MB | 2026-09-01

---
+++ medium/3568-minimum-moves-to-clean-the-classroom/solution.java
@@ -0,0 +1,25 @@
+                    sc = j;
+                }
+                    sr = i;
+                if (ch == 'S') {
+                char ch = classroom[i].charAt(j);
+
+            for (int j = 0; j < n; j++) {
+
+
+        for (int i = 0; i < m; i++) {
+            Arrays.fill(row, -1);
+        for (int[] row : id)
+
+        int[][] id = new int[m][n];
+        // Give each litter cell an ID
+
+        int litterCount = 0;
+        int sr = 0, sc = 0;
+
+        int n = classroom[0].length();
+        int m = classroom.length;
+
+
+    public int minMoves(String[] classroom, int energy) {
+class Solution {
