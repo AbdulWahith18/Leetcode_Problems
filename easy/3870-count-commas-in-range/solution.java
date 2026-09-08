333e98edb7b9f2cfc0d63e8874e2b17b4c5e72ac
AbdulWahith18 <abdulwahith0818@gmail.com>
2026-09-08 06:39:57 +0530

    ✅ Count Commas in Range | Easy | Java | Runtime: 0 ms | Memory: 42.3 MB | 2026-09-08

--- easy/3870-count-commas-in-range/solution.java
+++ easy/3870-count-commas-in-range/solution.java
@@ -1,5 +1,11 @@
 class Solution {
     public int countCommas(int n) {
-        return n/3;
+        int count=0;
+        while(n>0)
+        {
+            count++;
+            n/=10;
+        }
     }
+        return count/3;
 }
