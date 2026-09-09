dea3417b3ff722a95d0835a3f73ae28f07ec8188
AbdulWahith18 <abdulwahith0818@gmail.com>
2026-09-09 07:04:51 +0530

    ✅ Count Commas in Range II | Medium | Java | Runtime: 0 ms | Memory: 42.2 MB | 2026-09-09

---
+++ medium/3871-count-commas-in-range-ii/solution.java
@@ -0,0 +1,14 @@
+
+        // Numbers with 10 to 12 digits → 3 commas
+        if (n >= 1000000000L) {
+            ans += 3 * (Math.min(n, 999999999999L) - 999999999);
+        }
+        }
+            ans += 2 * (Math.min(n, 999999999) - 999999);
+        if (n >= 1000000) {
+        // Numbers with 7 to 9 digits → 2 commas
+
+        }
+            ans += Math.min(n, 999999) - 999;
+        if (n >= 1000) {
+        // Numbers with 4 to 6 digits → 1 comma
