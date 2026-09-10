27ebb4d6f8ffa94bfbc696d1136dde66b8d308f6
AbdulWahith18 <abdulwahith0818@gmail.com>
2026-09-10 18:51:58 +0530

    ✅ Shortest Distance to Target String in a Circular Array | Easy | Java | Runtime: 1 ms (96.73%) | Memory: 46.8 MB | 2026-09-10

--- easy/2515-shortest-distance-to-target-string-in-a-circular-array/solution.java
+++ easy/2515-shortest-distance-to-target-string-in-a-circular-array/solution.java
@@ -1,27 +1,21 @@
 class Solution {
-    public int minMirrorPairDistance(int[] nums) {
+    public int closestTarget(String[] words, String target, int startIndex) {
 
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int res = Integer.MAX_VALUE;
+        int n = words.length;
+        int ans = Integer.MAX_VALUE;
 
-        for (int i = nums.length - 1; i >= 0; i--) {
+        for (int i = 0; i < n; i++) {
 
-            int temp = nums[i];
-            int reverse = 0;
+            if (words[i].equals(target)) {
 
-            while (temp != 0) {
-                reverse = reverse * 10 + temp % 10;
-                temp /= 10;
+                int diff = Math.abs(i - startIndex);
+
+                int distance = Math.min(diff, n - diff);
+
+                ans = Math.min(ans, distance);
             }
-
-            if (map.containsKey(reverse)) {
-                res = Math.min(res, map.get(reverse) - i);
-            }
-
-            // Store this number as a possible nums[j]
-            map.put(nums[i], i);
         }
 
-        return res == Integer.MAX_VALUE ? -1 : res;
+        return ans == Integer.MAX_VALUE ? -1 : ans;
     }
 }
