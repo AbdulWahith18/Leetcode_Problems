0a29460bee981176402378fba234be3c7603aa14
AbdulWahith18 <abdulwahith0818@gmail.com>
2026-09-10 18:45:56 +0530

    ✅ Minimum Absolute Distance Between Mirror Pairs | Medium | Java | Runtime: 65 ms (58.99%) | Memory: 121.3 MB | 2026-09-10

--- medium/3761-minimum-absolute-distance-between-mirror-pairs/solution.java
+++ medium/3761-minimum-absolute-distance-between-mirror-pairs/solution.java
@@ -1,26 +1,27 @@
 class Solution {
     public int minMirrorPairDistance(int[] nums) {
-        HashMap<Integer,Integer> s=new HashMap<>();
-        for(int i=0;i<nums.length;i++)
-        {
-            if(!s.containsKey(nums[i]))
-                s.put(nums[i],i);
-        }
-        int res=Integer.MAX_VALUE;
-        for(int i=0;i<nums.length;i++)
-        {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int res = Integer.MAX_VALUE;
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+
+            int temp = nums[i];
             int reverse = 0;
 
-            while (nums[i] != 0) 
-            {
-            reverse = reverse * 10 + (nums[i] % 10);
-            nums[i] = nums[i] / 10;
+            while (temp != 0) {
+                reverse = reverse * 10 + temp % 10;
+                temp /= 10;
             }
-            if(s.containsKey(reverse)&&i<s.get(reverse))
-                res=Math.min(res,Math.abs(i-s.get(reverse)));
+
+            if (map.containsKey(reverse)) {
+                res = Math.min(res, map.get(reverse) - i);
+            }
+
+            // Store this number as a possible nums[j]
+            map.put(nums[i], i);
         }
-        if(res==Integer.MAX_VALUE)
-            return -1;
-        return res;
+
+        return res == Integer.MAX_VALUE ? -1 : res;
     }
 }
