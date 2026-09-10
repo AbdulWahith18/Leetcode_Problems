8bd59efe0245f29e77e8f68e68018a1a1e2c5672
AbdulWahith18 <abdulwahith0818@gmail.com>
2026-09-10 18:19:08 +0530

    ✅ Maximum Distance Between a Pair of Values | Medium | Java | Runtime: 6 ms | Memory: 53.4 MB | 2026-09-10

--- medium/1855-maximum-distance-between-a-pair-of-values/solution.java
+++ medium/1855-maximum-distance-between-a-pair-of-values/solution.java
@@ -2,7 +2,7 @@ class Solution {
     public int maxDistance(int[] nums1, int[] nums2) {
         int j=nums2.length-1;
         int i=nums1.length;
-        int max=Integer.MIN_VALUE;
+        int max=0;
         for(int x=j;x>=0;x--)
         {
             for(int y=0;y<i&&y<=j;y++)
