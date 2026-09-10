349a203e4bf09c5a76153850c5ced2e946aee504
AbdulWahith18 <abdulwahith0818@gmail.com>
2026-09-10 17:57:00 +0530

    ✅ Mirror Distance of an Integer | Easy | Java | Runtime: 1 ms (99.85%) | Memory: 42.8 MB | 2026-09-10

--- easy/3783-mirror-distance-of-an-integer/solution.java
+++ easy/3783-mirror-distance-of-an-integer/solution.java
@@ -1,13 +1,13 @@
-class Solution {
-    public int mirrorDistance(int num) {
-        int reverse = 0,x=num;
+class Solution {
+    public int mirrorDistance(int num) {
+        int reverse = 0,x=num;
 
-        while (num != 0)
-            int digit = num % 10;
-            reverse = reverse * 10 + digit;
-            num = num / 10;
-        }
-    }
-        {
-        return Math.abs(x-reverse);
+        while (num != 0)
+        {
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
+            num = num / 10;
+        }
+        return Math.abs(x-reverse);
+    }
 }
