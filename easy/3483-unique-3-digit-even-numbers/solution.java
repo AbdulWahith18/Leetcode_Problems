c30e892feddd325412d88dec963e260bc545cfb2
AbdulWahith18 <abdulwahith0818@gmail.com>
2026-09-11 07:25:58 +0530

    ✅ Unique 3-Digit Even Numbers | Easy | Java | Runtime: 0 ms | Memory: 42.6 MB | 2026-09-11

---
+++ easy/3483-unique-3-digit-even-numbers/solution.java
@@ -0,0 +1,26 @@
+    public int totalNumbers(int[] digits)
+        int disCount=0,count=0;
+        for(int i:digits)
+        {
+            if(i%2==0)
+                count++;
+        }
+            {
+        Set<Integer> s=new HashSet<>();
+                if(!s.contains(i))
+                    disCount++;
+            }     
+                s.add(i);
+        if(count==digits.length)
+            return disCount;
+    }
+        }
+        return res;
+    {
+            res*=i;
+        {
+        for(int i=2;i<=n;i++)
+    {
+        int res=1;
+    public int factorial(int n)
+class Solution {
