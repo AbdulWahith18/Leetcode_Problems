2701fb2aec68b351ed02702c46f07d325464b1b0
AbdulWahith18 <abdulwahith0818@gmail.com>
2026-09-13 17:15:59 +0530

    ✅ Minimum Number of Pushes to Type Word I | Easy | Java | Runtime: 0 ms | Memory: 42.7 MB | 2026-09-13

---
+++ easy/3014-minimum-number-of-pushes-to-type-word-i/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int res=word.length();
+        if(res<=8)
+            return res;
+        else if(res<=16)
+            return (8+(res%8)*2);
+        
+    }
+        else if(res<=24)
+            return (24+(res%8)*3);
+        else
+            return (48+(res%8)*4);
+}
